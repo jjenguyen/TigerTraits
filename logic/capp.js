c583809a22fe2ae2db67ac75e31b4a474ffb0f27
seanmoserr <seanmoserr@gmail.com>
2025-02-27 14:42:30 -0600

    Added basic files for developing quiz questions into logic branch

---
+++ logic/capp.js
@@ -0,0 +1,25 @@
+function getUser(talk){
+    talk.question('Welcome to Tiger Traits! What is your name? ', (uname) => {
+        console.log(`Nice to meet you, ${uname}!`);
+        talk.close();
+    });
+}
+
+
+
+function main(){
+    const readline = require('readline');
+
+    const talk = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    var type = "";
+    
+    //get username to store
+    getUser(talk);
+}
+
+main();
+
